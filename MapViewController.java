de49d512a3148a77aee360b4615b38bfa1a583c7
Harshraj Patel <k22018200@kcl.ac.uk>
2023-03-13 22:29:27 +0000

    added north london polygons

---
+++ MapViewController.java
@@ -0,0 +1,49 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.shape.Polygon;
+import javafx.fxml.Initializable;
+
+public class MapViewController{
+
+    @FXML
+    private Polygon barnetPolygon;
+
+    @FXML
+    private BorderPane bp;
+
+    @FXML
+    private Polygon enfieldPolygon;
+
+    @FXML
+    private DatePicker fromDatePicker;
+
+    @FXML
+    private Polygon harrowPolygon;
+
+    @FXML
+    private AnchorPane map;
+
+    @FXML
+    private Polygon redbridgePolygon;
+
+    @FXML
+    private DatePicker toDatePicker;
+
+    @FXML
+    private Polygon walthamPolygon;
+
+    @FXML
+    void dateChanged(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getData(MouseEvent event) {
+
+    }
+
+}
