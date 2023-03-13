f218ed05ff1cf83eeb69e7b9faf28b5d2eba7298
Ishab Ahmed <ishabahmed00@gmail.com>
2023-03-13 14:56:53 +0000

    Table displays when valid date chosen

--- DataViewer.java
+++ DataViewer.java
@@ -3,7 +3,6 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Parent;
 import javafx.fxml.FXMLLoader;
-import java.util.ArrayList;
 import java.net.URL;
 import javafx.event.ActionEvent;
 
@@ -14,15 +13,7 @@ import javafx.event.ActionEvent;
  * @version 2023.03.13
  */
 public class DataViewer extends Application
-{
-    private ArrayList<CovidData> data;
-    
-    @Override
-    public void init() throws Exception {
-        CovidDataLoader dataLoader = new CovidDataLoader();
-        data = dataLoader.load();
-    }
-    
+{   
     @Override
     public void start(Stage stage) throws Exception {
         URL url = getClass().getResource("MainWindow.fxml");
--- DataViewerController.java
+++ DataViewerController.java
@@ -1,10 +1,12 @@
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.scene.control.DatePicker;
-import java.text.SimpleDateFormat;
-import java.text.DateFormat;
 import java.time.LocalDate;
 import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.control.TableView;
+import java.util.ArrayList;
 
 /**
  * Controls logic of DataViewer
@@ -27,18 +29,42 @@ public class DataViewerController
     private Button rightButton;
     
     @FXML
+    private VBox welcomePane;
+    
+    @FXML
+    private Pane tablePane;
+    
+    @FXML
+    private TableView dataTable;
+    
+    private ArrayList<CovidData> data;
+    
+    public DataViewerController() {
+        CovidDataLoader dataLoader = new CovidDataLoader();
+        data = dataLoader.load();
+    }
+    
+    @FXML
     private void dateChanged(ActionEvent event) {
+        leftButton.setDisable(true);
         rightButton.setDisable(true);
-        
-        DateFormat dateFormat = new SimpleDateFormat("yy-mm-dd");
+        welcomePane.setVisible(true);
+        tablePane.setVisible(false);
         
         LocalDate fromDate = fromDatePicker.getValue();
         LocalDate toDate = toDatePicker.getValue();
         
         if (fromDate != null && toDate != null) {
             if (fromDate.isBefore(toDate)) {
+                leftButton.setDisable(false);
                 rightButton.setDisable(false);
+                welcomePane.setVisible(false);
+                tablePane.setVisible(true);
             }
         }
+    }
+    
+    private void loadTable() {
+        
     }
 }
