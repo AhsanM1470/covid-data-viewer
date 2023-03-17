fa759b5969946f82f54457de08d372d348544280
Ishab Ahmed <ishabahmed00@gmail.com>
2023-03-17 16:56:21 +0000

    Documented DataViewerController

--- DataViewerController.java
+++ DataViewerController.java
@@ -26,93 +26,107 @@ public class DataViewerController extends Controller
 {
     @FXML
     private BorderPane mainLayout;
-    
+
     @FXML
     private StackPane mainPanel;
-    
+
     @FXML
     private Button leftButton;
-    
+
     @FXML
     private Button rightButton;
-    
+
     @FXML
     private VBox welcomePane;
-    
+
     @FXML
     private Pane tablePane;
-    
+
     @FXML
     private TableView<CovidData> dataTable;
-    
+
     @FXML
     private Label dataTableInfoLabel;
-    
+
     private Controller[] controllers;
     private int controllerIndex;
 
+    /**
+     * Initializes the FXML controller class.
+     * This method is called by the FXMLLoader when the corresponding FXML file is loaded.
+     */
     @FXML
     public void initialize() {
-        
+
+        // Create TableColumns for the TableView
         TableColumn<CovidData,String> dateCol = new TableColumn<CovidData,String>("Date");
         dateCol.setCellValueFactory(new PropertyValueFactory<CovidData,String>("date"));
-         
+
         TableColumn<CovidData,String> boroughCol = new TableColumn<CovidData,String>("Borough");
         boroughCol.setCellValueFactory(new PropertyValueFactory<CovidData,String>("borough"));
-         
+
         TableColumn<CovidData,String> newCasesCol = new TableColumn<CovidData,String>("New Cases");
         newCasesCol.setCellValueFactory(new PropertyValueFactory<CovidData,String>("newCases"));
-        
+
         TableColumn<CovidData,String> totalCasesCol = new TableColumn<CovidData,String>("Total Cases");
         totalCasesCol.setCellValueFactory(new PropertyValueFactory<CovidData,String>("totalCases"));
-         
+
         TableColumn<CovidData,String> newDeathsCol = new TableColumn<CovidData,String>("New Deaths");
         newDeathsCol.setCellValueFactory(new PropertyValueFactory<CovidData,String>("newDeaths"));
-    
+
         TableColumn<CovidData,String> totalDeathsCol = new TableColumn<CovidData,String>("Total Deaths");
         totalDeathsCol.setCellValueFactory(new PropertyValueFactory<CovidData,String>("totalDeaths"));
-        
+
+        // Add the TableColumns to the TableView
         dataTable.getColumns().addAll(dateCol, boroughCol, newCasesCol, totalCasesCol, newDeathsCol, totalDeathsCol);
-        
-        // make all columns equal width
+
+        // Make all columns equal width
         dataTable.setColumnResizePolicy(dataTable.CONSTRAINED_RESIZE_POLICY);
-        
+
+        // Try to load the controllers for all the panels
         try {
             controllers = new Controller[2];
             loadControllers();
         } catch (Exception e) {
+            // Print the error message and stack trace to the console
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
-        
+
+        // Current controller to be used is in 0th index
         controllerIndex = 0;
     }
-    
+
     /**
-     * Loads the controllers of all of the panels.
+     * Loads the controllers of all of the panels in the app.
+     * 
+     * @throws Exception if the FXMLLoader fails to load any FXML file.
      */
     public void loadControllers() throws Exception {
-        
         FXMLLoader mapLoader = new FXMLLoader(getClass().getResource(
-               "MapWindow.fxml"));
+                    "MapWindow.fxml"));
         mapLoader.load();
         Controller mapController = mapLoader.getController();
         
-        controllers[0] = this;
         controllers[1] = mapController;
+        // Current controller responsible for first panel
+        controllers[0] = this;
     }
-    
+
     /**
-     * When the date is changed, check if the date range is valid and display the data if so.
+     * Handles the event when the date picker is changed. Updates the data table 
+     * with the chosen date range  if valid date chosen, otherwise shows an error message to user.
+     * 
+     * @param event The event triggered by changing the date picker.
      */
     @FXML
     private void dateChanged(ActionEvent event) {
         LocalDate fromDate = fromDatePicker.getValue();
         LocalDate toDate = toDatePicker.getValue();
-        
+
         // sets the pickers of the current panel to the dates chosen
         controllers[controllerIndex].setDateRange(fromDate, toDate);
-        
+
         if (isDateRangeValid(fromDate, toDate) == true) {
             if (isDataInDateRange(fromDate, toDate) == true) {
                 populateTable(fromDate, toDate);
@@ -123,30 +137,37 @@ public class DataViewerController extends Controller
         } else {
             dataTableInfoLabel.setText("The 'from' date is before the 'to' date.");
         }
-        
+
         setWelcomeState(false);
     }
 
-    
     /**
-     * @return whether data is in the range selected
+     * Checks if there is any CovidData objects within the date range provided.
+     * 
+     * @param from The start date of the range to be checked
+     * @param to The end date of the range to be checked
+     * @return bool indicating if there is CovidData within the date range
      */
     private boolean isDataInDateRange(LocalDate from, LocalDate to) {
         boolean dataInRange = false;
-        
+
         for (CovidData row : data) {
+            // date in CovidData is string, parse to LocalDate for easy comparison
             LocalDate date = LocalDate.parse(row.getDate());
+            // check if date is between 'from' and 'to'
             if (date.isAfter(from) && date.isBefore(to)) {
                 dataInRange = true;
                 break;
             } 
         }
-        
+
         return dataInRange;
     }
-    
+
     /**
-     * Toggles the Welcome components.
+     * Changes the state of the welcome pane.
+     * 
+     * @param state Bool indicating whether the welcome pane should be shown or hidden
      */
     private void setWelcomeState(boolean state) {
         leftButton.setDisable(state);
@@ -154,37 +175,46 @@ public class DataViewerController extends Controller
         welcomePane.setVisible(state);
         tablePane.setVisible(!state);
     }
-    
+
     /**
-     * Populates the table with the data in the range selected.
+     * Populates the table with CovidData objects that fall within the given date range.
+     * 
+     * @param from The starting date of the date range
+     * @param to The ending date of the date range
      */
     private void populateTable(LocalDate from, LocalDate to) {
+        // Clear any existing items from the table
         dataTable.getItems().clear();
         
+        // Add all CovidData objects within the given date range to the table
         for (CovidData d : data) {
             LocalDate date = LocalDate.parse(d.getDate());
             if (date.isAfter(from) && date.isBefore(to))
                 dataTable.getItems().add(d);
         }
     }
-    
+
     /**
-     * Changes the center panel to the next.
+     * Changes the center of the main layout to the next panel.
+     * 
+     * @param event The event triggered by clicking the next panel button
      */
     @FXML
     private void nextPanel(ActionEvent event) {
         controllerIndex++;
         controllerIndex = controllerIndex % controllers.length;
-        
-        // sets the date picker of the next panel to the dates chosen on the current panel
+
+        // Sets the date picker of the next panel to the dates chosen on the current panel
         controllers[controllerIndex].setDateRange(getFromDate(), getToDate());
-        
-        // switches the center of the BorderPane to the next panel
+
+        // Switches the center of the main layout to the next panel
         mainLayout.setCenter(controllers[controllerIndex].getView());
     }
-    
+
     /**
-     * Changes the center panel to the previous.
+     * Changes the center of the main layout to the previous panel.
+     * 
+     * @param event The event triggered by clicking the next panel button
      */
     @FXML
     private void previousPanel(ActionEvent event) {
@@ -192,39 +222,43 @@ public class DataViewerController extends Controller
         if (controllerIndex < 0) {
             controllerIndex = controllers.length - 1;
         }
-        
-        // sets the date picker of the previous panel to the dates chosen on the current panel
+
+        // Sets the date picker of the previous panel to the dates chosen on the current panel
         controllers[controllerIndex].setDateRange(getFromDate(), getToDate());
-        
-        // switches the center of the BorderPane to the previous panel
+
+        // Switches the center of the main layout to the previous panel
         mainLayout.setCenter(controllers[controllerIndex].getView());
     }
-    
+
     /**
-     * @return the value in the fromDatePicker
+     * @return the currently selected date in 'fromDatePicker'
      */
     public LocalDate getFromDate() {
         return fromDatePicker.getValue();
     }
-    
+
     /**
-     * @return the value in the toDatePicker
+     * @return the currently selected date in 'toDatePicker'
      */
     public LocalDate getToDate() {
         return toDatePicker.getValue();
     }
-    
+
     /**
-     * @return an array of all of the CovidData
+     * Returns the list of CovidData objects loaded from the data source.
+     * 
+     * @return ArrayList of CovidData objects
      */
     public ArrayList<CovidData> getData() {
         return data;
     }
-    
+
     protected void dateChanged(LocalDate from, LocalDate to) {;}
-    
+
     /**
-     * @return the main centre panel
+     * Returns the center panel of this view.
+     * 
+     * @return the main center panel
      */
     protected Parent getView() {
         return mainPanel;
